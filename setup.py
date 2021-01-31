757cf594624bde8ca192f3d5d7bd24f7ee29c222
darokin <darokin42@gmail.com>
2021-01-31 20:38:08 +0100

    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="darshellclock",
-    version="0.0.2",
+    version="1.0.0",
     python_requires='>=3.6.0',
     author="Adrien Rebuzzi",
     author_email="darokin42@gmail.com",
